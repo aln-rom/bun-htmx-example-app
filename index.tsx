0129f4b023f3c061a2c780c622a4e991b5b58a01
aln-rom <57873642+aln-rom@users.noreply.github.com>
2024-01-22 09:29:22 +0300

    configure server

--- index.tsx
+++ index.tsx
@@ -1,10 +1,16 @@
-Bun.serve({
+const server = Bun.serve({
     port: 8080,
     hostname: "localhost",
     fetch: handler
 });
 
+console.log(`Server is listening on http://${server.hostname}:${server.port}`)
+
 function handler(request: Request): Response {
     const url = new URL(request.url);
+
+    if (url.pathname === '' || url.pathname === '/')
+        return new Response(Bun.file("index.html"))
+
     return new Response('Not found', { status: 404 })
 }
